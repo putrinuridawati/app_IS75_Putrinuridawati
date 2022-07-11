c5a738a15d9c0cd2aa0b889af2d86cc4d885ff70
putrinuridawati <107973549+putrinuridawati@users.noreply.github.com>
2022-07-11 13:38:48 +0700

    menambahkan migration

---
+++ database/migrations/2022_07_06_041925_create_transaksis_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('transaksis', function (Blueprint $table) {
+            $table->id();
+            $table->string('kode_tran')->unique();
+            $table->date('tanggal ');
+            $table->string('kode_pelanggan');
+            $table->string('kode_kasir');
+            $table->string('kode_barang');
+            $table->string('brutto');
+            $table->string('tarra ');
+            $table->string('netto ');
+            $table->string('harga ');
+            $table->string('total ');
+            $table->string('bayar ');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('transaksis');
+    }
+};
--- database/migrations/2022_07_06_041947_create_barangs_table.php
+++ database/migrations/2022_07_06_041947_create_barangs_table.php
@@ -13,14 +13,11 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('barangs', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->enum('hak_akses', ['admin','user']);
-            $table->rememberToken();
+            $table->string('kode_barang')->unique();
+            $table->string('nama_barang');
+            $table->string('hargaperkg');
             $table->timestamps();
         });
     }
@@ -32,6 +29,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('barangs');
     }
 };
