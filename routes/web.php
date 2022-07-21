4315d5788aa1d683c9e5c072b80617b5856d74bf
putrinuridawati <107973549+putrinuridawati@users.noreply.github.com>
2022-07-21 19:54:34 +0700

    controller adn route

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,10 @@
 <?php
 
+
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\BarangController;
+use App\Http\Controllers\TransaksiController;
+use Illuminate\Support\Facades\Auth;
 
 /*
 |--------------------------------------------------------------------------
@@ -20,3 +24,17 @@ use Illuminate\Support\Facades\Route;
 Auth::routes();
 
 Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::get('/barang', [BarangController::class,'index']);
+Route::get('/barang/form', [BarangController::class,'create']);
+Route::post('/barang/store', [BarangController::class,'store']);
+Route::get('/barang/edit/{id}', [BarangController::class,'edit']);
+Route::put('/barang/{id}', [BarangController::class,'update']);
+Route::delete('/barang/{id}', [BarangController::class,'destroy']);
+
+Route::get('/transaksi', [TransaksiController::class,'index']);
+Route::get('/transaksi/form', [TransaksiController::class,'create']);
+Route::post('/transaksi/store', [TransaksiController::class,'store']);
+Route::get('/transaksi/edit/{id}', [TransaksiController::class,'edit']);
+Route::put('/transaksi/{id}', [TransaksiController::class,'update']);
+Route::delete('/transaksi/{id}', [TransaksiController::class,'destroy']);
